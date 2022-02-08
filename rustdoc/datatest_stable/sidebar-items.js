a3c3fb8ca5323bdd06647d7b67a9bb083df552de
sunshowers <rain@sunshowers.io>
2022-02-08 01:33:46 +0000

    Deploying to gh-pages from @ nextest-rs/nextest@f871657f49c4e0db781cc1303c5f79c074d2a6ae 🚀

--- rustdoc/datatest_stable/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"macro":[["harness","`datatest-stable` test harness entry point. Should be declared in the test module."]],"type":[["Result","The result type for `datatest-stable` tests."]]});
