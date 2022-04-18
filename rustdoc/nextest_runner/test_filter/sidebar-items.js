2af45cde0fd47cb6fa625cd660ecd30dd1acc33d
sunshowers <rain@sunshowers.io>
2022-04-18 16:47:31 +0000

    Deploying to gh-pages from @ nextest-rs/nextest@14d779108cefadaf6f97d0a6c1a66b0162c2efbf 🚀

--- rustdoc/nextest_runner/test_filter/sidebar-items.js
+++ rustdoc/nextest_runner/test_filter/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["RunIgnored","Whether to run ignored tests."]],"struct":[["TestFilter","Test filter, scoped to a single binary."],["TestFilterBuilder","A builder for `TestFilter` instances."]]});
+initSidebarItems({"enum":[["RunIgnored","Whether to run ignored tests."]],"struct":[["FilterInstance","Instance of a test to which a filter is being applied."],["TestFilter","Test filter, scoped to a single binary."],["TestFilterBuilder","A builder for `TestFilter` instances."]]});
